143b7cd1c29838b38684cbbe65928444efc57bad
hp <hp@venkat>
2021-02-04 10:57:32 +0530

    HTML to Servlet With Form validations

--- Proj05-Servlet-HtmlToServlet-UsingFormWithValidations/src/com/servlet/VoterApp.java
+++ Proj05-Servlet-HtmlToServlet-UsingFormWithValidations/src/com/servlet/VoterApp.java
@@ -14,55 +14,75 @@ public class VoterApp extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-	
-		List<String> errList= new ArrayList<String>();;
+
+		List<String> errList = new ArrayList<String>();
 		PrintWriter pw = null;
 		try {
 			pw = resp.getWriter();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		};
+		}
+		;
 		try {
 			resp.setContentType("text/html");
-		
+
 			String name = req.getParameter("name");
 			int age = Integer.parseInt(req.getParameter("age"));
 			String vflag = req.getParameter("vflag");
-			
-			if(vflag.equals("yes")) {
-			
-			pw.println("Election Commission of India");
-			
-			if(age>18)
-				pw.println("<h1 style='color:green;text-align:center'>Mr/Miss/Mrs "+name+" You are eligible for voting");
-			else
-				pw.println("<h1 style='color:green;text-align:center'>Mr/Miss/Mrs "+name+" You are not eligible for voting");
-			
-			pw.println("<h1><a href='input.html'>Home</a></h1>");
-			
-			pw.close();
+
+			if (vflag.equals("yes")) {
+
+				pw.println("Election Commission of India");
+
+				if (age > 18) {
+					pw.println("<h1 style='color:green;text-align:center'>Mr/Miss/Mrs " + name
+							+ " You are eligible for voting");
+				} else {
+					pw.println("<h1 style='color:green;text-align:center'>Mr/Miss/Mrs " + name
+							+ " You are not eligible for voting");
+
+					pw.println("<h1><a href='input.html'>Home</a></h1>");
+
+					pw.close();
+				}
 			} else {
-				
-				if(name.isBlank() || name.length()<6)
+
+				if (name.isBlank() || name.length() < 6)
 					errList.add("Name Should be greater than 5 chars");
-				else if(age==0)
+				else if (age == 0)
 					errList.add("Age should be greater than zero");
+				else {
+					pw.println("Election Commission of India");
+
+					if (age > 18) {
+						pw.println("<h1 style='color:green;text-align:center'>Mr/Miss/Mrs " + name
+								+ " You are eligible for voting");
+					} else {
+						pw.println("<h1 style='color:green;text-align:center'>Mr/Miss/Mrs " + name
+								+ " You are not eligible for voting");
+
+						pw.println("<h1><a href='input.html'>Home</a></h1>");
+
+						pw.close();
+
+				}
+			}
 			}
 		}catch (Exception e) {
 			errList.add("Please enter valid details");
 		}
-		
+
 		if (!errList.isEmpty()) {
-			errList.forEach(msg->{
+			errList.forEach(msg -> {
 				try {
-					resp.getWriter().println("<li><span style='color:red'>"+msg+"</span></li>");
+					resp.getWriter().println("<li><span style='color:red'>" + msg + "</span></li>");
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			});
 		}
-		
 	}
+	
 }
